1e477736c6b78f6674fdd5150b1ff3f1cdbf41a8
yoshizawaryosuke <88420159+yoshizawaryosuke@users.noreply.github.com>
2021-09-22 20:13:58 +0900

    モデル、カラム、アソシエーション作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_14_092706) do
+ActiveRecord::Schema.define(version: 2021_09_22_110207) do
+
+  create_table "stores", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "store_name", null: false
+    t.text "store_text", null: false
+    t.integer "store_noodle_id", null: false
+    t.integer "store_soup_id", null: false
+    t.integer "store_meat_id", null: false
+    t.integer "store_vegetable_id", null: false
+    t.integer "store_garlic_id", null: false
+    t.integer "store_oil_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_stores_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -31,4 +46,5 @@ ActiveRecord::Schema.define(version: 2021_09_14_092706) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "stores", "users"
 end
