05cd2fd5b8b0b85f8de771b3069939968a3ae831
yoshizawaryosuke <88420159+yoshizawaryosuke@users.noreply.github.com>
2021-09-22 15:54:11 +0900

    ER図、README修正

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 
 ## users テーブル
 
-| Column             | Type   | Options                 |
-| ------------------ | ------ | ----------------------- |
-| email              | string | null: false, unique:true|
-| encrypted_password | string | null: false             |
-| nickname           | string | null: false             |
+| Column             | Type    | Options                 |
+| ------------------ | ------  | ----------------------- |
+| email              | string  | null: false, unique:true|
+| encrypted_password | string  | null: false             |
+| nickname           | string  | null: false             |
 | noodle_id          | integer | null: false             |
 | soup_id            | integer | null: false             |
 | meat_id            | integer | null: false             |
@@ -21,32 +21,32 @@
 
 ## stores テーブル
 
-| Column        | Type       | Options                        |
-| --------------| ---------- | ------------------------------ |
-| store_name     | string     | null: false                    |
-| store_text     | text       | null: false                    |
-| store_noodle_id   | integer    | null: false                    |
-| postage_id    | integer    | null: false                    |
-| condition_id  | integer    | null: false                    |
-| prefecture_id  | integer    | null: false                    |
-| scheduled_delivery_id       | integer    | null: false                    |
-| price         | integer    | null: false                    |
-| user          | references | null: false, foreign_key: true |
+| Column             | Type       | Options                        |
+| -------------------| ---------- | ------------------------------ |
+| store_name         | string     | null: false                    |
+| store_text         | text       | null: false                    |
+| store_noodle_id    | integer    | null: false                    |
+| store_soup_id      | integer    | null: false                    |
+| store_meat_id      | integer    | null: false                    |
+| store_vegetable_id | integer    | null: false                    |
+| store_garlic_id    | integer    | null: false                    |
+| store_oil_id       | integer    | null: false                    |
+| user               | references | null: false, foreign_key: true |
 
 ### Association
 
 - belongs_to :user
-- has_one :buy
+- has_many :comments
 
-## buys テーブル
+## comments テーブル
 
-| Column    | Type       | Options                           |
-| --------- | ---------- | --------------------------------- |
-| user      | references | null: false, foreign_key: true    |
-| item      | references | null: false, foreign_key: true    |
+| Column       | Type       | Options                           |
+| ------------ | ---------- | --------------------------------- |
+| user         | references | null: false, foreign_key: true    |
+| item         | references | null: false, foreign_key: true    |
+| comment_text | text       | null: false                       |
 
 ### Association
 
 - belongs_to :user
-- belongs_to :item
-- has_one :shipping_address
+- belongs_to :store
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,143 @@
 require 'rails_helper'
-
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.build(:user)
+    sleep(1)
+  end
+
+  describe 'ユーザ新規登録' do
+    context '新規登録できるとき' do
+      it '全ての項目が入力されていれば登録できる' do
+        expect(@user).to be_valid
+      end
+      it 'passwordとpassword_confirmationが6文字以上で半角英数字混合であれば登録できる' do
+        @user.password = 'a00000'
+        @user.password_confirmation = 'a00000'
+        expect(@user).to be_valid
+      end
+    end
+
+    context '新規登録できないとき' do
+      it 'nicknameが空では登録できない' do
+        @user.nickname = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+      end
+      it 'emailが空では登録できない' do
+        @user.email = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email can't be blank")
+      end
+      it 'email@がないと登録できない' do
+        @user.email = 'hoge.com'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Email is invalid')
+      end
+      it '重複したemailが存在する場合登録できない' do
+        @user.save
+        another_user = FactoryBot.build(:user)
+        another_user.email = @user.email
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include('Email has already been taken')
+      end
+      it 'passwordが空では登録できない' do
+        @user.password = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password can't be blank")
+      end
+      it 'passwordが存在してもpassword_confirmationが空では登録できない' do
+        @user.password_confirmation = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+      it 'passwordが5文字以下では登録できない' do
+        @user.password = '00000'
+        @user.password_confirmation = '00000'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
+      end
+      it 'passwordは数字のみでは登録できない' do
+        @user.password = '000000'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+      it 'passwordは英字のみでは登録できない' do
+        @user.password = 'aaaaaa'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+      it 'passwordが存在してもpassword_confirmationが一致しなければ登録できない' do
+        @user.password = 'a00000'
+        @user.password_confirmation = 'b00000'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+      it 'passwordは全角では登録できない' do
+        @user.password = 'Ａ00000'
+        @user.password_confirmation = 'Ａ00000'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password is invalid')
+      end
+      it 'noodleが未選択だと保存できない' do
+        @user.noodle_id = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Noodle can't be blank")
+      end
+      it 'noodleが0だと保存できない' do
+        @user.noodle_id = 0
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Noodle can't be blank")
+      end
+      it 'soupが未選択だと保存できない' do
+        @user.soup_id = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Soup can't be blank")
+      end
+      it 'soupが0だと保存できない' do
+        @user.soup_id = 0
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Soup can't be blank")
+      end
+      it 'meatが未選択だと保存できない' do
+        @user.meat_id = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Meat can't be blank")
+      end
+      it 'meatが0だと保存できない' do
+        @user.meat_id = 0
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Meat can't be blank")
+      end
+      it 'vegetableが未選択だと保存できない' do
+        @user.vegetable_id = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Vegetable can't be blank")
+      end
+      it 'vegetableが0だと保存できない' do
+        @user.vegetable_id = 0
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Vegetable can't be blank")
+      end
+      it 'garlicが未選択だと保存できない' do
+        @user.garlic_id = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Garlic can't be blank")
+      end
+      it 'garlicが0だと保存できない' do
+        @user.garlic_id = 0
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Garlic can't be blank")
+      end
+      it 'oilが未選択だと保存できない' do
+        @user.oil_id = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Oil can't be blank")
+      end
+      it 'oilが0だと保存できない' do
+        @user.oil_id = 0
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Oil can't be blank")
+      end
+    end
+  end
 end
